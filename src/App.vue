bfdf082dde05970f63889b7aef0904f20a09863a
Ivanov Alexander <a.ivanov@uplab.ru>
2021-11-14 22:06:03 +0300

    add pagination and filter

--- src/App.vue
+++ src/App.vue
@@ -89,10 +89,27 @@
       </section>
 
       <template v-if="tickers.length">
+        <div>
+          <button
+            class="my-4 mx-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
+          >
+            Назад
+          </button>
+          <button
+            class="my-4 mx-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="hasNextPage"
+            @click="page = page + 1"
+          >
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter" /></div>
+        </div>
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -187,11 +204,22 @@ export default {
       sel: null,
       tickerList: [],
       autocompleteList: [],
-      error: false
+      error: false,
+      page: 1,
+      filter: "",
+      hasNextPage: true
     };
   },
 
   created: function() {
+    const windowData = Object.fromEntries(
+      new URL(window.location).searchParams.entries()
+    );
+
+    if (windowData.filter) this.filter = windowData.filter;
+
+    if (windowData.page) this.page = windowData.page;
+
     setTimeout(() => {
       this.setTickerList();
     }, 1000);
@@ -206,6 +234,18 @@ export default {
   },
 
   methods: {
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+
+      const filteredTickers = this.tickers.filter(t =>
+        t.name.toUpperCase().includes(this.filter.toUpperCase())
+      );
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
     subscribeToUpdates(tickerName) {
       setInterval(async () => {
         const f = await fetch(
@@ -213,7 +253,7 @@ export default {
         );
         const data = await f.json();
         this.tickers.find(t => t.name === tickerName).price =
-          data.USD > 1 ? data.USD.toFixed(2) : data.USD.toPrecision(2);
+          data.USD > 1 ? data.USD?.toFixed(2) : data.USD?.toPrecision(2);
 
         if (this.sel?.name === tickerName) {
           this.graph.push(data.USD);
@@ -254,6 +294,8 @@ export default {
         this.autocompleteList = [];
       }
 
+      this.filter = "";
+
       const newTicker = {
         name: this.ticker,
         price: "-"
@@ -281,6 +323,24 @@ export default {
       const minValue = Math.min(...this.graph);
       return this.graph.map(
         price => 10 + ((price - minValue) * 90) / (maxValue - minValue)
+      );
+    }
+  },
+
+  watch: {
+    filter() {
+      this.page = 1;
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
+      );
+    },
+    page() {
+      window.history.pushState(
+        null,
+        document.title,
+        `${window.location.pathname}?filter=${this.filter}&page=${this.page}`
       );
     }
   }
